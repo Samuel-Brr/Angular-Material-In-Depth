694c6bdf8a9cc2354b4e034fc997329fad8b6f17
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-22 06:20:42 +0100

    Writing Responsive CSS for Angular Material Components

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -19,6 +19,8 @@ export class CoursesCardListComponent implements OnInit {
 
     rowHeight = "500px"
 
+    handsetPortrait = false
+
     constructor(private dialog: MatDialog,
                 private responsive: BreakpointObserver) {
     }
@@ -37,6 +39,8 @@ export class CoursesCardListComponent implements OnInit {
 
           this.rowHeight = "500px"
 
+          this.handsetPortrait = false
+
           const breakpoints = result.breakpoints;
 
           if (breakpoints[Breakpoints.TabletPortrait]){
@@ -44,6 +48,7 @@ export class CoursesCardListComponent implements OnInit {
           }else if(breakpoints[Breakpoints.HandsetPortrait]){
             this.cols =1
             this.rowHeight = "430px"
+            this.handsetPortrait = true
           }else if(breakpoints[Breakpoints.HandsetLandscape]){
             this.cols = 1
           }else if(breakpoints[Breakpoints.TabletLandscape]){
