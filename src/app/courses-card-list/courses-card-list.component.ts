ba7e0dc3046a0d14e42c9935c6c6457a90edc919
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-22 06:09:05 +0100

    The Angular Material Responsive Breakpoint Observer Service

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
     selector: 'courses-card-list',
@@ -14,10 +15,42 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor(private dialog: MatDialog) {
+    cols = 3
+
+    rowHeight = "500px"
+
+    constructor(private dialog: MatDialog,
+                private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
+
+      this.responsive.observe([
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape
+      ])
+        .subscribe(result => {
+
+          this.cols = 3
+
+          this.rowHeight = "500px"
+
+          const breakpoints = result.breakpoints;
+
+          if (breakpoints[Breakpoints.TabletPortrait]){
+            this.cols = 1
+          }else if(breakpoints[Breakpoints.HandsetPortrait]){
+            this.cols =1
+            this.rowHeight = "430px"
+          }else if(breakpoints[Breakpoints.HandsetLandscape]){
+            this.cols = 1
+          }else if(breakpoints[Breakpoints.TabletLandscape]){
+            this.cols = 2
+          }
+
+        })
 
     }
 
