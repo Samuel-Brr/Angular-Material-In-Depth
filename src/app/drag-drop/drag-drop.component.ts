fef35a18f0d8ce1e2c9b81b9e3c6a091167ddd2f
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-22 05:03:30 +0100

    Angular Material Drag and Drop Between Different Lists

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -90,6 +90,27 @@ export class DragDropComponent {
     }
   ];
 
+  done = []
+
+  dropMultiList(event: CdkDragDrop<Lesson[]>){
+
+    if(event.previousContainer == event.container){
+
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+
+    }else{
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      )
+
+    }
+
+  }
+
   drop(event: CdkDragDrop<Lesson[]>){
 
     moveItemInArray(this.lessons, event.previousIndex, event.currentIndex)
